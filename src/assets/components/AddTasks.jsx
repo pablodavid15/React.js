3a0bb5401b8f240e9de5fabec8f3d9ed35d4d5ac
Pablo David <144298884+pablodavid15@users.noreply.github.com>
2025-04-02 21:03:41 -0300

    Delete src directory

--- src/assets/components/AddTasks.jsx
+++
@@ -1,7 +0,0 @@
-function AddTasks() {
-    return(
-        <h1>Add Tasks</h1>
-    );
-}
-
-export default AddTasks
--- src/assets/components/Tasks.jsx
+++
@@ -1,8 +0,0 @@
-function Tasks() {
-    //
-        return(
-            <h1>Tasks</h1>
-        )
-};
-
-export default Tasks;
